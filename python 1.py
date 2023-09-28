85a98d6ef29d16322b96f3eed2aa2ea67591e376
Sanjayking2001 <137169050+Sanjayking2001@users.noreply.github.com>
2023-09-28 15:28:29 +0530

    Add files via upload

---
+++ python 1.py
@@ -0,0 +1,11 @@
+gender = input("Gender? ")
+gender = gender.lower()
+if gender == "male":
+    print("Your cat is male")
+elif gender == "female":
+    print("Your cat is female")
+else:
+    print("Invalid input")
+
+
+nationality
